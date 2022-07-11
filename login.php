1090919f1892a6f378d1157d314168e8318f6260
Lawrence <loreganlloyd@gmail.com>
2022-07-11 17:23:44 -0400

    completed styling and created login page

--- login.php
+++ login.php
@@ -20,21 +20,17 @@
             <div class="header">
             <img src="assets/images/mooflixlogo.png" title="logo" alt="MooFlix">
 
-                <h3>Sign Up</h3>
+                <h3>Sign In</h3>
                 <span>to continue to MooFlix</span>
             </div>
-            <form id="signUpForm" method="POST">
-                <input type="text" name="firstName" placeholder="First name" required>
-                <input type="text" name="lastName" placeholder="Last name" required>
+            <form id="signUpForm" method="POST">                
                 <input type="text" name="username" placeholder="Username" required>
-                <input type="email" name="email" placeholder="Email" required>
-                <input type="email" name="email2" placeholder="Confirm Email" required>
+                
                 <input type="password" name="password" placeholder="Password" required>
-                <input type="password" name="password2" placeholder="Confirm Password" required>
                 <input type="submit" name="submitButton" value="SUBMIT">
             </form>
 
-            <a href="login.php" class="signInMessage">Already have an account? Sign in here.</a>
+            <a href="register.php" class="signInMessage">Need an account? Sign up here.</a>
 </div>
     </div>
 </body>
