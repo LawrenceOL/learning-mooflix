2701d968015ea8e1c7051e18cd3e339f834fda4b
Lawrence <loreganlloyd@gmail.com>
2022-08-30 09:49:49 -0400

    entity previews correct video instead of random

---
+++ entity.php
@@ -0,0 +1,12 @@
+<?php
+require_once("includes/Header.php");
+
+if (!isset($_GET["id"])) {
+    exit("No ID passed into page");
+}
+
+$entityId = $_GET["id"];
+$entity = new Entity($con, $entityId);
+
+$preview = new PreviewProvider($con, $userLoggedIn);
+echo $preview->createPreviewVideo($entity);
