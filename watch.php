187c82cd446f0039e0914330818af227c1bd1f82
Lawrence <loreganlloyd@gmail.com>
2022-09-16 11:45:11 -0400

    create videoControls back button and style

--- watch.php
+++ watch.php
@@ -11,8 +11,13 @@ $video->incrementViews();
 
 <div class="watchContainer">
 
-    <video controls autoplay>
+    <div class="videoControls watchNav">
+        <button><i class="fa-solid fa-arrow-left"></i></button>
+        <h1><?php echo $video->getTitle(); ?></h1>
 
+    </div>
+
+    <video controls autoplay>
         <source src='<?php echo $video->getFilePath(); ?>' type="video/mp4">
     </video>
 
