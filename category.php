785ab6996c3285b4a001250ab0d93173ce1d8817
Lawrence <loreganlloyd@gmail.com>
2022-10-10 14:51:26 -0400

    preview entity for category

---
+++ category.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("includes/Header.php");
+
+if (!isset($_GET["id"])) {
+    ErrorMessage::show("No id passed to page");
+}
+
+$preview = new PreviewProvider($con, $userLoggedIn);
+echo $preview->createCategoryShowPreviewVideo($_GET["id"]);
+
+$containers = new CategoryContainers($con, $userLoggedIn);
+echo $containers->showMovieCategories();
