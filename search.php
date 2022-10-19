d1c0c1809c5d3a998aa0cd2827d0931742b4be27
Lawrence <loreganlloyd@gmail.com>
2022-10-19 19:50:21 -0400

    create and style search box

---
+++ search.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once("includes/Header.php");
+?>
+
+
+<div class="textboxContainer">
+    <input type="text" class="searchInput" placeholder="search for something">
+</div>
+
+<div class="results"></div>
