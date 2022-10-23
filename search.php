697ef19bc8f63b44e6f2513ca80de0fbf48393f1
Lawrence <loreganlloyd@gmail.com>
2022-10-23 12:24:07 -0400

    console log search input using jquery and php

--- search.php
+++ search.php
@@ -9,3 +9,22 @@ include_once("includes/Header.php");
 </div>
 
 <div class="results"></div>
+
+<script>
+$(function() {
+
+    let username = '<?php echo $userLoggedIn; ?>';
+    let timer;
+
+    $(".searchInput").keyup(function() {
+        clearTimeout(timer);
+
+
+        timer = setTimeout(function() {
+            let val = $(".searchInput").val();
+            console.log();
+        }, 500);
+    })
+
+})
+</script>
