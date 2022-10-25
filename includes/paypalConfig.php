b5fbacfeb740dad60efb7326b79fd78b7ddca14c
Lawrence <loreganlloyd@gmail.com>
2022-10-25 19:33:12 -0400

    create billing plan

--- includes/paypalConfig.php
+++ includes/paypalConfig.php
@@ -1,2 +1,9 @@
 <?php
-require_once("../");
+require_once("./paypal/autoload.php");
+
+$apiContext = new \PayPal\Rest\ApiContext(
+    new \PayPal\Auth\OAuthTokenCredential(
+        $PAYPAL_CLIENT_ID,     // ClientID
+        $PAYPAL_CLIENT_SECRET      // ClientSecret
+    )
+);
