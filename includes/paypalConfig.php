b4da3a35ce772d6d65ddd9715d5f5023d85646be
Lawrence <loreganlloyd@gmail.com>
2022-10-25 19:40:18 -0400

    fix git ignore

---
+++ includes/paypalConfig.php
@@ -0,0 +1,11 @@
+<?php
+require_once("./paypal/autoload.php");
+
+$apiContext = new \PayPal\Rest\ApiContext(
+    new \PayPal\Auth\OAuthTokenCredential(
+        $PAYPAL_CLIENT_ID,     // ClientID
+        $PAYPAL_CLIENT_SECRET      // ClientSecret
+    )
+);
+
+//test 
